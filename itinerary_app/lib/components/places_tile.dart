821da49f6efaf4b6fc3b25d627fd1bb088909ccc
Mahesh1772 <87927591+Mahesh1772@users.noreply.github.com>
2024-01-21 05:45:55 +0800

    FInalized location tiles

--- itinerary_app/lib/components/places_tile.dart
+++ itinerary_app/lib/components/places_tile.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:itinerary_app/components/location_class.dart';
@@ -96,29 +97,92 @@ class PlacesTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-              child: Image.network(
-                location.image,
-                height: 110,
-                width: double.infinity,
-                fit: BoxFit.cover,
+      child: NeumorphicBox(
+        child: Padding(
+          padding: EdgeInsets.all(8.0.sp),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20.r),
+                    child: Image.network(
+                      location.image,
+                      height: 110,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(height: 20.h),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 150.w,
+                            child: AutoSizeText(
+                              location.name,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.sp,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 150.w,
+                            child: AutoSizeText(
+                              location.category,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15.sp,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10.h),
+                          Icon(Icons.wb_sunny_outlined, size: 30.sp),
+                        ],
+                      ),
+                    ],
+                  ),
+                  // You can add more widgets here to display additional information
+                ],
               ),
-            ),
-            ListTile(
-              title: Text(location.name,
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-            ),
-            // You can add more widgets here to display additional information
-          ],
+              Row(
+                children: [
+                  //Icon(Icons.wb_sunny_outlined, size: 30.sp),
+                  //SizedBox(height: 10.h),
+                  Padding(
+                    padding: EdgeInsets.only(top: 215.0.h, left: 150.0.w),
+                    child: Container(
+                      alignment: Alignment.bottomRight,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8.w, vertical: 15.h),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15.r),
+                          bottomRight: Radius.circular(15.r),
+                        ),
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 20.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
