09c9dacfa073031a552978229314635fa32d65dc
Aak242 <110832731+Aak242@users.noreply.github.com>
2024-01-21 01:45:27 +0800

    Revert "Merge branch 'rishit'"
    This reverts commit b1f04bf681405dc7bfaa624574707771342f8f73, reversing
    changes made to f22fc8d31bba43bcda7e4ce04f12d5157cd4148c.

--- itinerary_app/lib/main.dart
+++ itinerary_app/lib/main.dart
@@ -6,10 +6,18 @@ import 'package:itinerary_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
